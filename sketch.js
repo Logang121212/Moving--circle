2566bf59dfa4927e17d9cdb882a8e0975069919e
Logang121212 <124134601+Logang121212@users.noreply.github.com>
2023-02-01 10:28:26 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+//function is a box of code that does things that are connected
+function setup() {
+  createCanvas(600, 600)
+  
+}
+
+function draw() {
+   noStroke();
+  fill("yellow");
+  background(225, 20, 90);
+  circle(mouseX, mouseY, 20);
+}
